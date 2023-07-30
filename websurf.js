dfc570e037cbb5815175d339e49ed171bc17339e
cytfloar <cytfloar@gmail.com>
2023-07-29 20:58:58 -0400

    show number fixed

--- websurf.js
+++ websurf.js
@@ -15,8 +15,8 @@ const psychoJS = new PsychoJS({
   debug: true
 });
 
-let expName = 'WebSurf'; 
-let expInfo = {'participant': '', 'session': '001', "group": ""};
+let expName = 'WebSurf';
+let expInfo = { 'participant': '', 'session': '001', "group": "" };
 
 psychoJS.start({
   expName: expName,
@@ -41,8 +41,8 @@ async function quitPsychoJS(message, isCompleted) {
     psychoJS.experiment.nextEntry();
   }
   psychoJS.window.close();
-  psychoJS.quit({message: message, isCompleted: isCompleted});
-  
+  psychoJS.quit({ message: message, isCompleted: isCompleted });
+
   return Scheduler.Event.QUIT;
 }
 
@@ -54,149 +54,168 @@ function timeFrom(start) {
 }
 
 async function showOffer(sec, keys, logo = PRACLOGO) {
-    var answer = {}
-    clearScreen()
-    newInstruction(INSTD.format(sec), {y: -0.3})
-    var skipButton = newImage(SKIP, {style: {
-      width: "200px", height: "150px"}, x: 0.3, y: 0.45})
-    var stayButton = newImage(STAY, {style: {
-      width: "200px", height: "150px"}, x: -0.3, y: 0.45})
-    newImage(logo, {style: {
-      width: "150px", height: "150px"}, y: -0.65})
-    var bar = newProgressBar(sec, {y: -0.05});
-    var key, start = new Date();
-    if (keys === 'Digit2') key = await KeyPress(keys);
-    else {
-      key = await KeyPress(keys);
-      answer.reactionTime = timeFrom(start)
-      answer.keyPress = key.code.slice(-1)
-      if (key.code === 'Digit1') {
-        skipButton.style.display = "none"
-        stayButton.style.display = "none"
-        bar()
-        newImage(QUIT, {style: {
-          width: "200px", height: "150px"}, x: 0.3, y: 0.45})
-        var quitKey = await KeyPress('Digit2', sec)
-        answer.code = quitKey.code === 'Digit2' ? "Quit" : "Stay"
-      } else answer.code = "Skip";
-    }
-    answer.offsetOfOffer = timeFrom(start);
-    return answer;
+  var answer = {}
+  clearScreen()
+  newInstruction(INSTD.format(sec), { y: -0.3 })
+  var skipButton = newImage(SKIP, {
+    style: {
+      width: "200px", height: "150px"
+    }, x: 0.3, y: 0.45
+  })
+  var stayButton = newImage(STAY, {
+    style: {
+      width: "200px", height: "150px"
+    }, x: -0.3, y: 0.45
+  })
+  newImage(logo, {
+    style: {
+      width: "150px", height: "150px"
+    }, y: -0.65
+  })
+  var bar = newProgressBar(sec, { y: -0.05 });
+  var key, start = new Date();
+  if (keys === 'Digit2') key = await KeyPress(keys);
+  else {
+    key = await KeyPress(keys);
+    answer.reactionTime = timeFrom(start)
+    answer.keyPress = key.code.slice(-1)
+    if (key.code === 'Digit1') {
+      skipButton.style.display = "none"
+      stayButton.style.display = "none"
+      bar()
+      newImage(QUIT, {
+        style: {
+          width: "200px", height: "150px"
+        }, x: 0.3, y: 0.45
+      })
+      var quitKey = await KeyPress('Digit2', sec)
+      answer.code = quitKey.code === 'Digit2' ? "Quit" : "Stay"
+    } else answer.code = "Skip";
+  }
+  answer.offsetOfOffer = timeFrom(start);
+  return answer;
 }
 
 // test var breakList = [0, 40, 60, 140, 999999], blockNum = 0; 
-var breakList = [0, 480, 960, 1440, 999999], blockNum = 0; 
+var breakList = [0, 480, 960, 1440, 999999], blockNum = 0;
 var baseTime;
 var nTrial = 1;
 
 async function showNumbers() {
   var numbers = []
-  for (var i = 0; i < 5; ++ i) {
-      var number = randint(1, 4)
-      numbers.push(number)
-      newInstruction(number, {
-        style: { fontSize: "70px" },
-        x: uniform(-0.8, 0.8),
-        y: uniform(-0.8, 0.8)
-      })
-      await KeyPress("Digit" + number)
-      clearScreen()
+  for (var i = 0; i < 5; ++i) {
+    var number = randint(1, 4)
+    numbers.push(number)
+    newInstruction(number, {
+      style: { fontSize: "70px" },
+      x: uniform(-0.8, 0.8),
+      y: uniform(-0.8, 0.8)
+    })
+    await KeyPress("Digit" + number)
+    clearScreen()
   }
   return numbers
 }
 
 async function main() {
+  clearScreen()
+  await manyInstructions([INSTA, INSTB, INSTC])
+  await showOffer(5.0, 'Digit2')
+  clearScreen()
+  newInstruction(INSTF)
+  clearScreen()
+  await showNumbers()
+  clearScreen()
+  newInstruction(INSTG)
+  await KeyPress('Space')
+  clearScreen()
+  var status = await showOffer(5.0, ['Digit1', 'Digit2'])
+  clearScreen()
+  await manyInstructions([INSTH, INSTI, INSTJ])
+
+  var perm = []
+  for (var i = 0; i < 4; ++i) {
+    perm.push(Array.from({ length: 62 }, (_, index) => index + 1));
+    shuffleArray(perm[i])
+  }
+  var counter = [0, 0, 0, 0]
+
+  baseTime = Date.now()
+
+  console.log(perm);
+
+  while (true) {
+    addData("trial", nTrial)
+    addData("onset of trial", timeFrom(baseTime))
+
+    // if (timeFrom(baseTime) >= breakList[blockNum]) {
+    //   newInstruction("+", {style: {fontSize: "100px", fontWeight: "bold"}})
+    //   await Timer(40) //test only for 1s, change to 40s for final copy
+    //   clearScreen()
+    //   blockNum++
+    // }
+
+    var cue = nTrial % 4
+    var delay = randint(3, 30)
+    // if (cue === 1) shuffleArray(BLOCKORDER)
+
+    addData("delay", delay)
+
+    var logo = BLOCKORDER[cue].Logo, order = BLOCKORDER[cue].Order
+    var vidnum = perm[cue][counter[cue]++]
+
+    addData("onset of offer", timeFrom(baseTime))
+    var reaction = await showOffer(delay, ['Digit1', 'Digit2'], logo)
     clearScreen()
-    await manyInstructions([INSTA, INSTB, INSTC])
-    await showOffer(5.0, 'Digit2')
-    clearScreen()
-    newInstruction(INSTF)
-    clearScreen()
-    await showNumbers()
-    clearScreen()
-    newInstruction(INSTG)
-    await KeyPress('Space')
-    clearScreen()
-    var status = await showOffer(5.0, ['Digit1', 'Digit2'])
-    clearScreen()
-    await manyInstructions([INSTH, INSTI, INSTJ])
+    addData("offset of offer", timeFrom(baseTime))
+    addData("cuelogo", order)
+    addData("video number", vidnum)
+    addData("keypress", reaction.keyPress)
+    addData("decision", reaction.code)
+    addData("rt decision", reaction.reactionTime)
 
-    var perm = []
-    for (var i = 0; i < 4; ++ i) {
-      perm.push(Array.from({length: 62}, (_, index) => index + 1));
-      shuffleArray(perm[i])
-    }
-    var counter = [0, 0, 0, 0]
+    if (reaction.code === 'Stay') {
+      var clipStart = Date.now()
+      addData("onset of clip", timeFrom(baseTime))
+      var videoDOM = newVideo(VIDPATH.format(order, vidnum), {
+        y: -0.05, style: { width: "1000px", height: "562.5px" }
+      });
+      newImage(logo, {
+        style: {
+          width: "150px", height: "150px"
+        }, y: -0.65
+      })
+      await VideoEnded(videoDOM)
+      addData("offset of clip", timeFrom(baseTime))
+      addData("duration of clip", timeFrom(clipStart))
+      var scale = newRatingScale(4, { y: 0.5 })
+      var scaleResponse = await scale()
 
-    baseTime = Date.now()
+      addData("rating", scaleResponse.code)
+      addData("rt rating", scaleResponse.reactionTime)
 
-    while (true) {
-      addData("trial", nTrial)
-      addData("onset of trial", timeFrom(baseTime))
-
-      // if (timeFrom(baseTime) >= breakList[blockNum]) {
-      //   newInstruction("+", {style: {fontSize: "100px", fontWeight: "bold"}})
-      //   await Timer(40) //test only for 1s, change to 40s for final copy
-      //   clearScreen()
-      //   blockNum++
-      // }
-
-      var cue = nTrial % 4
-      var delay = randint(3, 30)
-      // if (cue === 1) shuffleArray(BLOCKORDER)
-
-      addData("delay", delay)
-
-      var logo = BLOCKORDER[cue].Logo, order = BLOCKORDER[cue].Order
-      var vidnum = perm[cue][counter[cue]++]
-
-      addData("onset of offer", timeFrom(baseTime))
-      var reaction = await showOffer(delay, ['Digit1', 'Digit2'], logo)
       clearScreen()
-      addData("offset of offer", timeFrom(baseTime))
-      addData("cuelogo", order)
-      addData("keypress", reaction.keyPress)
-      addData("decision", reaction.code)
-      addData("rt decision", reaction.reactionTime)
-
-      if (reaction.code === 'Stay') {
-        var clipStart = Date.now()
-        addData("onset of clip", timeFrom(baseTime))
-        var videoDOM = newVideo(VIDPATH.format(order, vidnum), {
-          y: -0.05, style: { width: "1000px", height: "562.5px" }
-        });
-        newImage(logo, {style: {
-        width: "150px", height: "150px"}, y: -0.65})
-        await VideoEnded(videoDOM)
-        addData("offset of clip", timeFrom(baseTime))
-        addData("duration of clip", timeFrom(clipStart))
-        var scale = newRatingScale(4, {y: 0.5})
-        var scaleResponse = await scale()
-
-        addData("rating", scaleResponse.code)
-        addData("rt rating", scaleResponse.reactionTime)
-
-        clearScreen()
-        addData("onset of numbers", timeFrom(baseTime))
-        var numbers = await showNumbers()
-        addData("travelcost", numbers)
-        addData("cost key", numbers)
-        addData("offset of numbers", timeFrom(baseTime))
-      }
-      nTrial++;
-      addData("offset of trial", timeFrom(baseTime))
-
-      if (timeFrom(baseTime) > 35 * 60) { //change to 35*60
-        clearScreen()
-        newInstruction(INSTK)
-        await Timer(20) //change to 20 later
-        clearScreen()
-        break 
-      }
-
-      nextEntry()
     }
-    quitPsychoJS(INSTK, true);
+    nTrial++;
+    addData("onset of numbers", timeFrom(baseTime))
+    var numbers = await showNumbers()
+    addData("travelcost", numbers)
+    addData("cost key", numbers)
+    addData("offset of numbers", timeFrom(baseTime))
+
+    addData("offset of trial", timeFrom(baseTime))
+
+    if (timeFrom(baseTime) > 35 * 60) { //change to 35*60
+      clearScreen()
+      newInstruction(INSTK)
+      await Timer(20) //change to 20 later
+      clearScreen()
+      break
+    }
+
+    nextEntry()
+  }
+  quitPsychoJS(INSTK, true);
 
 }
 
@@ -209,4 +228,4 @@ async function experiment() {
   return main()
 }
 
-psychoJS.scheduleCondition(function() { return (psychoJS.gui.dialogComponent.button === 'OK'); }, experiment);
+psychoJS.scheduleCondition(function () { return (psychoJS.gui.dialogComponent.button === 'OK'); }, experiment);
